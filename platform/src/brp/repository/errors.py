2b68c3a04e4fb32e3f16ba7c9d0014b5db7233e8
ToanLM1 <toanlm1@cnspartner.com>
2026-07-10 19:08:47 +0700

    feat(platform): T-202 add revision repository service

---
+++ platform/src/brp/repository/errors.py
@@ -0,0 +1,25 @@
+"""Typed repository errors mapped to API problem responses later."""
+
+
+class RepositoryError(Exception):
+    """Base class for governed repository failures."""
+
+
+class DecisionNotFoundError(RepositoryError):
+    pass
+
+
+class RevisionNotFoundError(RepositoryError):
+    pass
+
+
+class ApprovedRevisionNotFoundError(RepositoryError):
+    pass
+
+
+class AmbiguousEffectiveRevisionError(RepositoryError):
+    pass
+
+
+class InvalidEffectiveIntervalError(RepositoryError):
+    pass
